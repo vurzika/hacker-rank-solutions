5224efb4400fbf3ceb4532be3376e2b0be3d2d80
Viktorija Urzika <viktorija.urzika@gmail.com>
2020-05-12 21:34:11 -0700

    Added Day 5 solution

---
+++ src/thirtydaysofcoding/Day5.java
@@ -0,0 +1,23 @@
+package thirtydaysofcoding;
+
+import java.util.Scanner;
+
+public class Day5 {
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void printMultipliers(int n) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(n + " x " + i + " = " + n * i);
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        scanner.close();
+
+        printMultipliers(n);
+    }
+}
