3b3a24400058fe8e3b7a89e7e86536fe045f4fcf
Viktorija Urzika <viktorija.urzika@gmail.com>
2020-05-15 22:00:56 -0700

    Added Day 8 solution

---
+++ src/thirtydaysofcoding/Day8.java
@@ -0,0 +1,32 @@
+package thirtydaysofcoding;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Day8 {
+
+    public static void main(String[] argh) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+
+        Map<String, Integer> numberMap = new HashMap<>();
+        for (int i = 0; i < n; i++) {
+            String name = in.next();
+            int phone = in.nextInt();
+            numberMap.put(name, phone);
+            // Write code here
+        }
+        while (in.hasNext()) {
+            String s = in.next();
+            // Write code here
+            if (numberMap.containsKey(s)) {
+                System.out.println(s + "=" + numberMap.get(s));
+            } else {
+                System.out.println("Not found");
+            }
+
+        }
+        in.close();
+    }
+}
